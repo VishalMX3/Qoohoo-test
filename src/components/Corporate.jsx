0cc67a7f3f1475172c509a9d0550f219386a7af4
Vishal Pundhir <63143819+VishalMX3@users.noreply.github.com>
2023-01-25 15:42:22 +0530

    Added Responsiveness for Mobile Screen

--- src/components/Corporate.jsx
+++ src/components/Corporate.jsx
@@ -18,8 +18,8 @@ export default function Corporate() {
           <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolore similique fuga ipsa suscipit maiores ex ipsam. Possimus iure quo quis, omnis at ratione aliquam nam itaque! Corporis suscipit sequi numquam!</p>
           <button className='c-button2'><p>Join Now -{'>'}</p></button>
           </div>
-          <div className="c-img">
-            <img src={Random} alt="" />
+          <div >
+            <img src={Random} className="c-img" alt="" />
           </div>
         </div>
         
